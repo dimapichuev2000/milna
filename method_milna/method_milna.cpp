4d8bf1a03cfda46dd5f5066f430c0c0a185710c2
dimapichuev2000 <55379930+dimapichuev2000@users.noreply.github.com>
2020-05-21 14:25:31 +0900

    last ver

--- method_milna/method_milna.cpp
+++ method_milna/method_milna.cpp
@@ -34,38 +34,39 @@ void Out(std::vector<float> array)
 int main()
 {
 	setlocale(0, "");
-	float h = 0.1,
-		b = 1,
+	int n = 10, k = 0;
+	float
+		x0 = 0,
+		xN = 1,
 		eps = 0.0001,
-		a = 0;
+		h = (xN - x0) / n;
 	std::vector<float>y;
 	std::vector<float>x;
-	float c;
-	float d;
-	float y1;
-	float y2;
-	float e;
-	int n = 10;
-	while (true) {
+	float a, b, c, d, s, y1, y2, e;
+	while (k < n + 1) 
+	{
 		x = generateArray(n + 1, h);
 		y = zeros(n + 1);
 		y[0] = 1;
-		for (int k = 1; k < 4; k++)
+		
+		for (int k = 0; k < 4; k++)
 		{
-			a = h * f(x[k], y[k]),
-				b = h * f(x[k] + (h / 2), y[k] + (a / 2)),
-				c = h * f(x[k] + (h / 2), y[k] + (b / 2)),
-				d = h * f(x[k] + h, y[k] + c);
-			y[k + 1] = y[k] + (a + (2 * b) + (2 * c) + d) / 6.;
+			a = h * f(x[k], y[k]);
+			b = h * f(x[k] + (h / 2), y[k] + (a / 2));
+			c = h * f(x[k] + (h / 2), y[k] + (b / 2));
+			d = h * f(x[k] + h, y[k] + c);
+			y[k+1] = y[k] + (a + (2 * b) + (2 * c) + d) / 6.;
 		}
-		for (int k = 4; k < n + 1; k++)
+		
+		k = 4;
+		
+		while (k < n + 1)
 		{
-			y1 = y[k - 4] + (4 * h) / 3 * (2 * f(x[k - 3], y[k - 3]) - f(x[k -
-				2], y[k - 2]) + 2 * f(x[k - 1], y[k - 1]));
-			y2 = y[k - 2] + (h / 3) * (f(x[k - 2], y[k - 2]) + 4 * f(x[k - 1],
-				y[k - 1]) + f(x[k], y1));
+			y1 = y[k - 4] + (4 * h) / 3 * (2 * f(x[k - 3], y[k - 3]) - f(x[k - 2], y[k - 2]) + 2 * f(x[k - 1], y[k - 1]));
+			y2 = y[k - 2] + (h / 3) * (f(x[k - 2], y[k - 2]) + 4 * f(x[k - 1], y[k - 1]) + f(x[k], y1));
 			e = abs(y2 - y1) / 29.;
-			if (e < eps) {
+			if (e < eps) 
+			{
 				y[k] = y2;
 			}
 			else
@@ -74,13 +75,14 @@ int main()
 				n *= 2;
 				break;
 			}
-			if (k == n) {
-				break;
-			}
+			k++;
 		}
-		Out(x);
-		Out(y);
-		break;
+		if (k == n) 
+		{
+			break;
+		}
 	}
+	Out(x);
+	Out(y);
 	system("pause");
 }
